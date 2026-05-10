9e24865be433e2afb4d7cab08b8374b984b9f61f
Nicholas Kern <nkern.astro@gmail.com>
2026-05-10 16:06:45 -0400

    updated versioning system

--- cosmodiff/__init__.py
+++ cosmodiff/__init__.py
@@ -1,6 +1,9 @@
+from importlib.metadata import version as _pkg_version
+
 from . import utils
 from . import augment
 from . import optim
 
 from .optim import train, generate
-from .version import __version__
+
+__version__ = _pkg_version("cosmodiff")
--- cosmodiff/version.py
+++
@@ -1,4 +0,0 @@
-__version__ = "0.0.1"
-
-def version():
-    return "cosmodiff v{}".format(__version__)
